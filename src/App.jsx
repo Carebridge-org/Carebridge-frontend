5fdbb0566b316ff0b404427d25f5df91e891f15b
Valdemar <158381907+ValdemarLarsen02@users.noreply.github.com>
2025-11-17 03:45:00 +0100

    Oprettelse af Login flow
    Oprettet design og forbindelse til backend
    
    OBS: mangler korrekt prod url til backen noget data bør også fjernes fra localstorage (role) bør tjekkes server side

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Routes, Route, Link } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Contact from './pages/Contact'
-
+import Login from "./pages/(auth)/Login"
 function App() {
     return (
         <>
@@ -26,6 +26,7 @@ function App() {
                     <Route path="/" element={<Home />} />
                     <Route path="/about" element={<About />} />
                     <Route path="/contact" element={<Contact />} />
+                    <Route path="/auth/login" element={<Login />} />
                 </Routes>
             </Container>
         </>
